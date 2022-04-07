6efb5f4f0b0fa1b9b65ff08e9cd889c9a43b01ed
Zarema <50046942+Starburned7@users.noreply.github.com>
2022-04-08 00:22:53 +0300

    Update Window_fuction_exercises.sql

--- exercises/Window_fuction_exercises.sql
+++ exercises/Window_fuction_exercises.sql
@@ -16,4 +16,16 @@ with cte_rank as(select ticker, market_date, price, volume, RANK() OVER (
   where price_rank <=5
   order by ticker, price_rank
   
--- 3. Calculate a 7 day rolling average for the price and volume columns in the trading.prices table for each ticker.
+-- 3. Calculate a 7 day rolling average for the price and column in the trading.prices table for each ticker.
+
+with cte_moving_averages as(select ticker,market_date, price, AVG(price) OVER (
+  PARTITION BY ticker
+  ORDER BY market_date
+  RANGE BETWEEN '7 DAYS' PRECEDING AND CURRENT ROW
+) AS moving_avg_price
+
+from trading.prices)
+
+select * from cte_moving_averages
+where market_date between '2021-08-01' and '2021-08-10'
+order by ticker, moving_avg_price
